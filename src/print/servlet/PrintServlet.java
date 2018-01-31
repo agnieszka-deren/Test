79287529056a94720f864b55fbdcda6b449b489d
Agnieszka Dereń <agandruch@gmail.com>
2018-01-31 18:53:33 +0100

    Adding link.jsp

--- src/print/servlet/PrintServlet.java
+++ src/print/servlet/PrintServlet.java
@@ -13,8 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PrintServlet")
 public class PrintServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	
+
       public PrintServlet() {
     	  super();
       }
